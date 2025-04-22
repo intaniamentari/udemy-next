cdef92d9654175687ed47e7ec91850b78b64ea1f
intan <intaniamentari@gmail.com>
2025-04-22 09:47:28 +0800

    [3] Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import Image from "next/image";
 
 export default function Homepage() {
   return (
     <div className="flex flex-col gap-5">
-      <h1>Homepage</h1>
-      <Button className="w-max">Salon and Spa</Button>
-      <Input placeholder="Enter your name" className="w-max" />
+      {/* Hrader */}
+      <div className="flex justify-between items-center bg-gray-200 p-5 px-20">
+        <h1 className="font-bold! text-2xl text-black">B.E.A.U.T.Y</h1>
+        <Button>Login</Button>
+      </div>
+
+      {/* hero section */}
+      <div className="bg-white mt-20 lg:grid-cols-2 grid-cols-1 px-20 min-h-[70vh] items-center grid gap-10">
+        {/* content */}
+        <div className="col-span-1">
+          <div className="flex flex-col gap-5">
+            <h1 className="text-2xl font-bold!">Welcome to B.E.A.U.T.Y Salon and Spa</h1>
+            <p className="text-sm text-gray-600">B.E.A.U.T.Y Salon and Spa is a modern and luxurious spa experience. Helps you relax and rejuvenate. Have a experience of a lifetime. Enjoy your time with us.</p>
+            <Button className="w-max">Find a Salon</Button>
+          </div>
+        </div>
+        {/* image */}
+        <div className="col-span-1 flex justify-center">
+          <img className="w-full" src="images/salon.jpg" alt="salon" />
+        </div>
+      </div>
     </div>
   );
 }
