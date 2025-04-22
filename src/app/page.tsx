485dddbf9ecc0d6f4619c93e6773109d71957772
intan <intaniamentari@gmail.com>
2025-04-22 09:21:52 +0800

    [1][2] setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <div className="flex flex-col gap-5">
+      <h1>Homepage</h1>
+      <Button className="w-max">Salon and Spa</Button>
+      <Input placeholder="Enter your name" className="w-max" />
+    </div>
   );
 }
