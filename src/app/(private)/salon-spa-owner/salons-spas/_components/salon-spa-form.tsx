0f2ed029e1845de3283c9d6768e52954954a6326
intan <intaniamentari@gmail.com>
2025-04-23 10:52:49 +0800

    [21] create folder structure and reuseable components

---
+++ src/app/(private)/salon-spa-owner/salons-spas/_components/salon-spa-form.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface SalonSpaFormProps {
+    initialValues?: any
+    formType?: 'add' | 'edit'
+}
+
+function SalonSpaForm() {
+  return (
+    <div className='mt-7'>SalonForm</div>
+  )
+}
+
+export default SalonSpaForm
