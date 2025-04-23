2b4af057a8798f358f2f5c61393276e95e4e77e5
intan <intaniamentari@gmail.com>
2025-04-23 09:51:49 +0800

    [19] create interface salon spa

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -7,3 +7,27 @@ export interface IUser {
     isActive: boolean,
     createdAt: string
 }
+
+export interface ISalonSpa {
+    id: number,
+    name: string,
+    description: string,
+    address: string,
+    city: string,
+    state: string,
+    zip: string,
+    min_price: number,
+    max_price: number,
+    offer_status: string,
+    working_days: string[],
+    start_time : string,
+    end_time : string,
+    break_start : string,
+    break_end : string,
+    slot_duration : number,
+    max_booking_per_slot : number,
+    location_name : string,
+    latitude : number,
+    longitude : number,
+    createdAt: string
+}
